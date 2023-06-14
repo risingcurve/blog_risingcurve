118cff65b21e94ff895eac789b298db613f48b3e
risingcurve <gksdnjswhd10@naver.com>
2023-06-14 18:44:21 +0900

    :hammer: RecentPost.tsx 수정

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import { Post } from "../.contentlayer/generated/types";
 
-const RecentPosts = ({ posts }: { posts: Post[] }) => {
+const RecentPosts = ({ posts }) => {
   return (
     <section className={`mt-10`}>
       <h1 className={`text-3xl font-extrabold`}>최근 게시물</h1>
@@ -11,11 +10,12 @@ const RecentPosts = ({ posts }: { posts: Post[] }) => {
             key={post._id}
             href={`/blog/${post._raw.flattenedPath}`}
             passHref
-            className="mt-5"
           >
+            <a className="mt-5">
               <div className={`font-medium text-xl`}>{post.title}</div>
               <div className={`font-light`}>{post.description}</div>
-        </Link>
+            </a>
+          </Link>
         ))}
       </div>
     </section>
