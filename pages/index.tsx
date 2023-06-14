dc88641ccd2f0f76b5d1b9d4dd79dd4524a0b3d6
risingcurve <gksdnjswhd10@naver.com>
2023-06-15 03:44:10 +0900

    :lipstick: index.tsx 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,11 +20,11 @@ const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
             objectFit="cover"
             className={`rounded-3xl`}
           />
-          <span
-            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-9xl text flex justify-center w-full drop-shadow-lg`}
+          {/* <span
+            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-6xl text flex justify-center w-full drop-shadow-lg`}
           >
             {metadata.title}
-          </span>
+          </span> */}
         </div>
         <RecentPosts posts={posts} />
       </div>
