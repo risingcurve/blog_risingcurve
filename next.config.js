d04066142540f920e5ee2701f95ed99bf64569de
risingcurve <gksdnjswhd10@naver.com>
2023-06-14 18:55:33 +0900

    :hammer: next.config.js 재수정

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@
 // module.exports = nextConfig
 
 
-import { withContentlayer } from "next-contentlayer";
+const { withContentlayer } = require("next-contentlayer");
 
-export default withContentlayer({
+module.exports = withContentlayer({
   reactStrictMode: true,
 });
