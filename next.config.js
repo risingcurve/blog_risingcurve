ba91da48dee0e665495f7561203e1d8641b92131
risingcurve <gksdnjswhd10@naver.com>
2023-06-14 19:50:44 +0900

    :hammer: next.config.js 수정

--- next.config.js
+++ next.config.js
@@ -2,14 +2,23 @@
 // const nextConfig = {}
 
 // module.exports = nextConfig
+const { withContentlayer } = require('next-contentlayer')
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+      },
+}
 
+module.exports = withContentlayer(nextConfig)
 
-const { withContentlayer } = require("next-contentlayer");
+// const { withContentlayer } = require("next-contentlayer");
 
 // import { withContentlayer } from 'next-contentlayer'
 
 // export default withContentlayer({ })
 
-module.exports = withContentlayer({
-  reactStrictMode: true,
-});
+// module.exports = withContentlayer({
+//   reactStrictMode: true,
+// });
