fb8e4a8a52f5ca477f0756eae69ce7f982c15d47
risingcurve <gksdnjswhd10@naver.com>
2023-06-15 02:43:01 +0900

    :sparkles: contentlayer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+module.exports = withContentlayer({
+  reactStrictMode: true,
+});
