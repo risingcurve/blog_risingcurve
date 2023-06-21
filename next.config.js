f0a01380508fea3b69ea98288542f82443d2b769
risingcurve <gksdnjswhd10@naver.com>
2023-06-22 05:19:09 +0900

    :rewind: GA 전으로 롤백

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
 const { withContentlayer } = require("next-contentlayer");
+
 
 const nextConfig = {
   env: {
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
   },
-  reactStrictMode: true,
 };
 
-module.exports = withContentlayer(nextConfig);
-=======
-/** @type {import('next').NextConfig} */
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withContentlayer } = require('next-contentlayer');
-
 module.exports = withContentlayer({
+  nextConfig,
   reactStrictMode: true,
 });
->>>>>>> parent of 1cf2694 (Merge pull request #26 from risingcurve/google-analytics)
