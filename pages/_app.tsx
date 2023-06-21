c997f41f385a140b99256cda69095052c0043490
risingcurve <gksdnjswhd10@naver.com>
2023-06-22 03:51:36 +0900

    :hammer: 구글 애널리틱스 주석 처리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,23 +6,23 @@ import { useRouter } from 'next/router'
 import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+  // useEffect(() => {
+  //   const handleRouteChange = (url: URL) => {
+  //     gtag.pageview(url);
+  //   };
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+  //   router.events.on('hashChangeComplete', handleRouteChange);
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange);
+  //     router.events.off('hashChangeComplete', handleRouteChange);
+  //   };
+  // }, [router.events]);
 
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      {/* Global Site Tag (gtag.js) - Google Analytics
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
@@ -40,7 +40,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             });
           `,
         }}
-      />
+      /> */}
       <Component {...pageProps} />
     </>
   );
