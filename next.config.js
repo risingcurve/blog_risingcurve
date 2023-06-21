fb23898527e1726873b5d9b22eef7de743615408
risingcurve <gksdnjswhd10@naver.com>
2023-06-22 02:37:34 +0900

    :sparkles: Google Analytics 적용

--- next.config.js
+++ next.config.js
@@ -6,3 +6,11 @@ const { withContentlayer } = require('next-contentlayer');
 module.exports = withContentlayer({
   reactStrictMode: true,
 });
+
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
+  },
+};
+
+module.exports = nextConfig;
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -12,12 +12,14 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   };
 
   return (
-    <Container customMeta={customMeta}>
-      <div className="mt-10 prose">
-        <h1 className="text-sky-700">{post.title}</h1>
-        <MDXComponent />
-      </div>
-    </Container>
+    <div className="flex items-center content-center justify-center">
+      <Container customMeta={customMeta}>
+        <div className="mt-10 prose">
+          <h1 className="text-sky-700">{post.title}</h1>
+          <MDXComponent />
+        </div>
+      </Container>
+    </div>
   );
 };
 
